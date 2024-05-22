985f653d6fe5ece2b9cf1c31392f30279c0b7494
Siva70136 <cheerlasivamallikharjuna@.com>
2024-05-22 10:04:05 +0530

    Implemented infinate scroll

--- src/components/cardcontainer/index.js
+++ src/components/cardcontainer/index.js
@@ -1,36 +1,11 @@
-import { useEffect, useState } from 'react';
 import Cards from '../card';
 //https://cdn.furrl.in/vibes/VibeCard_HomeHunts.jpg
 
 
 const CardContainer = () => {
-  const [info, setInfo] = useState([]);
-  const url = 'https://api.furrl.in/api/v2/listing/getListingProducts';
-  const getData = async () => {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify({ "input": { "page": 1, "pageSize": 10, "filters": [], "id": "#HomeHunts", "entity": "vibe" } })
-    }
-    const response = await fetch(url, options);
-    if (response.ok) {
-      const data = await response.json();
-      const data1 = data.data.getListingProducts;
-      //console.log(data1);
-      setInfo(data1.products);
-      //console.log(pr);
-    }
-
-  }
-
-  useEffect(() => {
-    getData();
-  }, [])
   return (
     <div className=''>
-      <Cards data={info} />
+      <Cards  />
     </div>
   )
 }
